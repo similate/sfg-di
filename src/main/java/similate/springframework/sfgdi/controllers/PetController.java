b534832ed54dbdc3774fb670e52a2adaa39ec2e1
Doug Williams <similate@gmail.com>
2021-01-28 19:08:13 -0700

    Lesson 41; showing spring and bean lifecycle.

---
+++ src/main/java/similate/springframework/sfgdi/controllers/PetController.java
@@ -0,0 +1,22 @@
+package similate.springframework.sfgdi.controllers;
+
+
+import similate.springframework.sfgdi.services.PetService;
+import org.springframework.stereotype.Controller;
+
+/*
+ * Created by jt on 12/28/19.
+ */
+@Controller
+public class PetController {
+
+    private final PetService petService;
+
+    public PetController(PetService petService) {
+        this.petService = petService;
+    }
+
+    public String whichPetIsTheBest(){
+        return petService.getPetType();
+    }
+}
---
+++ src/main/java/similate/springframework/sfgdi/services/PetService.java
@@ -0,0 +1,9 @@
+package similate.springframework.sfgdi.services;
+
+/**
+ * Created by jt on 12/28/19.
+ */
+public interface PetService {
+
+    String getPetType();
+}
